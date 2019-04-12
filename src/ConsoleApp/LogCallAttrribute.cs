acf6ac2e8ad14e248daf734ec3d30ad9931151e5
Yang <yrzhang@thoughtworks.com>
2019-04-12 11:33:10 +0800

    write a sample

---
+++ src/ConsoleApp/LogCallAttrribute.cs
@@ -0,0 +1,16 @@
+﻿using AspectInjector.Broker;
+using System;
+
+namespace ConsoleApp
+{
+    [Aspect(Scope.Global)]
+    [Injection(typeof(LogCall))]
+    class LogCall : Attribute
+    {
+        [Advice(Kind.Before)]
+        public void LogEnter([Argument(Source.Name)] string name)
+        {
+            Console.WriteLine($"Calling {name}");   //you can debug it	
+        }
+    }
+}
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Calculate();
+
+            Console.ReadKey();
+        }
+
+        [LogCall]
+        private static void Calculate()
+        {
+            Console.WriteLine("Calculate");
+        }
+
+    }
+}
